f45b2eddc33bc860f7388b8767c0a4ab04b64946
kenshin-yasha <newtotheleague@gmail.com>
2026-01-13 16:42:40 +0530

    Added JS and CSS for delete button

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+/* Script for cards */
 const cardContainer = document.querySelector(".card-container");
 
 cardContainer.addEventListener("click", (event) => {
-    const card = event.target.closest("card");
+    const card = event.target.closest(".card");
 
     if(!card) return;
 
-    const cards = document.querySelectorAll("card");
+    const cards = document.querySelectorAll(".card");
     cards.forEach((c) => c.classList.remove("active"));
 
-    c.classList.add("active");
+    card.classList.add("active");
+});
+
+/* script for delete buttons */
+const deleteButtons = document.querySelectorAll(".delete-btn");
+
+deleteButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        event.stopPropagation();
+        const card = button.closest(".card");
+        card.remove();
+    });
 });
